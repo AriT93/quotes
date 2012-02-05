cf92ddd0af339b27bc7ebc34e558decafa7f081e
AriT93 <arit93@yahoo.com>
2012-02-05 15:43:44 -0600

    initial commit
    worked around enough for now.  need to save and work on the backbone
    parts of this.  but hey stock quotes!

---
+++ js/yhooquote.js
@@ -0,0 +1,30 @@
+function getUrl(symbol){
+    var q = escape('select * from yahoo.finance.quotes where symbol ="' + symbol +'"');
+    var str = "http://query.yahooapis.com/v1/public/yql?q=" + q + "&format=json&env=http://datatables.org/alltables.env";
+
+    return str;
+
+};
+
+
+function parseData(result){
+    var quotes = result.query.results.quote;
+    alert(quotes);
+    $("#symbol").html(quotes.Symbol);
+    $("#price").html(quotes.LastTradePriceOnly);
+    $("#range").html(quotes.YearRange);
+//  $("#data").html(JSON.stringify( result.query.results));
+}
+
+
+$(document).ready(function(){
+
+
+
+
+$("#quote").click(function(){
+    $.getJSON(getUrl("ibm"),parseData);
+    //alert("blarrrr")
+});
+
+});
