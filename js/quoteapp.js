50ff74c951cf737bbbe06901f53c90211bfae7c7
AriT93 <arit93@yahoo.com>
2012-02-08 14:32:07 -0600

    everything works now it needs a bunch of refactoring to make it right

--- js/quoteapp.js
+++ js/quoteapp.js
@@ -1,7 +1,12 @@
 $(document).ready(function (){
 
     window.Quote = Backbone.Model.extend({
-        symbol: null
+        symbol: null,
+        clear: function(){
+            this.destroy();
+//            $(this.id.el).remove();
+        }
+
     });
 
     window.QuoteList = Backbone.Collection.extend({
@@ -13,7 +18,31 @@ $(document).ready(function (){
 
     window.Quotes = new QuoteList;
 
-    AppView = Backbone.View.extend({
+    window.QuoteView = Backbone.View.extend({
+        tagName: "li",
+
+        template: _.template($("#quote-template").html()),
+
+        events: {
+            "click .symbol-destroy": "clear"
+        },
+        initialize: function(){
+            this.model.view = this;
+        },
+        render: function(){
+            $(this.el).html(this.template(this.model));
+            $(this.el).attr('id', getModelID(this.model.Symbol).id);
+            return this;
+        },
+        clear: function(){
+            Quotes.get(this.el.id).clear();
+            $("#quote-list #"+this.el.id).fadeOut('slow',function(el){
+            $(el).remove();
+            });
+        }
+    });
+
+    window.AppView = Backbone.View.extend({
         el: $("body"),
         initialize: function(){
             Quotes.fetch();
@@ -28,22 +57,21 @@ $(document).ready(function (){
                 quotelist.push( model.get('symbol'));
             });
             $.getJSON(getUrl(quotelist),parseData);
-            setTimeout(function(){
-                appview.render();
-            },60000);
         },
         addsymbol: function(data){
             Quotes.create(data);
             this.render();
         },
         showPrompt: function(){
-            var symbol = prompt("What Symbol");
+            var symbol = prompt("What Symbol").toUpperCase();
             var quote_model = new Quote({symbol: symbol});
             Quotes.create(quote_model);
             this.render();
         },
-        addFriendLi: function(model){
-            this.render();
+        addQuoteLi: function(model){
+            var view = new QuoteView({model: model});
+            var li = view.render().el;
+            $("#quote-list").append(view.render().el);
         }
     });
     window.appview = new AppView;
