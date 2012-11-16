3085afa5b0bc470c77e1f450827bcafd603709b1
AriT93 <arit93@yahoo.com>
2012-11-16 13:17:20 -0600

    now with yahoo goodness

--- js/googleFin.js
+++ js/googleFin.js
@@ -7,9 +7,11 @@ $(document).ready(function() {
         {
             model: Quote,
             // Url to request when fetch() is called
-            url: 'http://www.google.com/finance/info?infotype=infoquoteall&q=',
+            // //%22YHOO%22%2C%22AAPL%22%2C%22GOOG%22%2C%22MSFT%22
+            //url: 'http://www.google.com/finance/info?infotype=infoquoteall&q=' + localStorage.getItem("quotes").toString(),
+            url: 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20('+'"'+localStorage.getItem("quotes").toString().replace(/,/g,'","') + '"'+ ')&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
             parse: function(response) {
-                return response;
+                return response.query.results.quote;
             },
             // Overwrite the sync method to pass over the Same Origin Policy
             sync: function(method, model, options) {
@@ -17,11 +19,13 @@ $(document).ready(function() {
                 var params = _.extend({
                     type: 'GET',
                     dataType: 'jsonp',
-                    url: that.url + localStorage.getItem("quotes").toString(),
+                    url: that.url, // + localStorage.getItem("quotes").toString(),
                     processData: false
                 }, options);
-
                 return $.ajax(params);
+                // options.timeout = 10000;
+                // options.dataType = 'jsonp xml';
+                // var qs= Backbone.sync(method,model,options);
             },
             remove: function(deleted){
                 var arSym = localStorage.getItem("quotes").split(',');
@@ -45,6 +49,8 @@ $(document).ready(function() {
                 symbols: that.symbols,
                 success: function () {
                     that.render();
+                },
+                error: function(req,error,errorThrown){
                 }
             });
             setTimeout(that.updateQuotes,60000);
