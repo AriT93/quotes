83aea679c864d326e79e5e08e91aadb32746869f
Ari Turetzky <AriT93@yahoo.com>
2014-05-30 09:14:26 -0700

    Update googleFin.js

--- js/googleFin.js
+++ js/googleFin.js
@@ -16,7 +16,7 @@ $(document).ready(function() {
                 var that = this;
                 var params = _.extend({
                     type: 'GET',
-                    dataType: 'jsonp',
+                    dataType: 'script',
                     url: that.url + localStorage.getItem("quotes").toString(),
                     processData: false
                 }, options);
